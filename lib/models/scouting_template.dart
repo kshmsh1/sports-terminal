425d5b2f9a31208753d86c59e87888dc2bedaa3c
Keshav Ramesh <63920717+kshmsh1@users.noreply.github.com>
2026-05-16 12:27:09 -0500

    Add scouting template model

---
+++ lib/models/scouting_template.dart
@@ -0,0 +1,19 @@
+class ScoutingTemplate {
+  const ScoutingTemplate({
+    required this.id,
+    required this.section,
+    required this.priority,
+    required this.status,
+    required this.description,
+    required this.evidenceNeeded,
+    required this.output,
+  });
+
+  final String id;
+  final String section;
+  final String priority;
+  final String status;
+  final String description;
+  final String evidenceNeeded;
+  final String output;
+}
