7a6221e9bfecf9fe8fd9560de3cc4fc2ac225522
Keshav Ramesh <63920717+kshmsh1@users.noreply.github.com>
2026-05-16 01:30:40 -0500

    Add JSON parsing to draft pick

--- lib/models/draft_pick.dart
+++ lib/models/draft_pick.dart
@@ -13,6 +13,22 @@ class DraftPick {
     this.asOf,
   });
 
+  factory DraftPick.fromJson(Map<String, dynamic> json) {
+    return DraftPick(
+      id: json['id'] as String,
+      draftYear: json['draftYear'] as int,
+      round: json['round'] as int?,
+      pickNumber: json['pickNumber'] as int?,
+      teamId: json['teamId'] as String?,
+      playerId: json['playerId'] as String?,
+      playerName: json['playerName'] as String?,
+      schoolOrClub: json['schoolOrClub'] as String?,
+      country: json['country'] as String?,
+      sourceId: json['sourceId'] as String?,
+      asOf: json['asOf'] as String?,
+    );
+  }
+
   final String id;
   final int draftYear;
   final int? round;
