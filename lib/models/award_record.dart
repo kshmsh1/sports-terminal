70a9874fffaf88e02ba6cb59689e5ef090d3967b
Keshav Ramesh <63920717+kshmsh1@users.noreply.github.com>
2026-05-16 01:29:35 -0500

    Add JSON parsing to award record

--- lib/models/award_record.dart
+++ lib/models/award_record.dart
@@ -13,6 +13,22 @@ class AwardRecord {
     this.asOf,
   });
 
+  factory AwardRecord.fromJson(Map<String, dynamic> json) {
+    return AwardRecord(
+      id: json['id'] as String,
+      awardName: json['awardName'] as String,
+      seasonId: json['seasonId'] as String,
+      playerId: json['playerId'] as String?,
+      teamId: json['teamId'] as String?,
+      rank: json['rank'] as int?,
+      votesFirstPlace: json['votesFirstPlace'] as int?,
+      points: (json['points'] as num?)?.toDouble(),
+      share: (json['share'] as num?)?.toDouble(),
+      sourceId: json['sourceId'] as String?,
+      asOf: json['asOf'] as String?,
+    );
+  }
+
   final String id;
   final String awardName;
   final String seasonId;
