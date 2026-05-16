a3758d012a0a8dd31b8037647386d66104ddd4cb
Keshav Ramesh <63920717+kshmsh1@users.noreply.github.com>
2026-05-16 16:27:39 -0500

    Add core box and efficiency fields to team season stat model

--- lib/models/team_season_stat.dart
+++ lib/models/team_season_stat.dart
@@ -13,6 +13,17 @@ class TeamSeasonStat {
     this.offensiveRating,
     this.defensiveRating,
     this.netRating,
+    this.personalFoulsPerGame,
+    this.fieldGoalPercentage,
+    this.threePointPercentage,
+    this.freeThrowPercentage,
+    this.effectiveFieldGoalPercentage,
+    this.trueShootingPercentage,
+    this.turnoversPerGame,
+    this.reboundsPerGame,
+    this.assistsPerGame,
+    this.stealsPerGame,
+    this.blocksPerGame,
     this.sourceId,
     this.asOf,
   });
@@ -32,6 +43,17 @@ class TeamSeasonStat {
       offensiveRating: (json['offensiveRating'] as num?)?.toDouble(),
       defensiveRating: (json['defensiveRating'] as num?)?.toDouble(),
       netRating: (json['netRating'] as num?)?.toDouble(),
+      personalFoulsPerGame: (json['personalFoulsPerGame'] as num?)?.toDouble(),
+      fieldGoalPercentage: (json['fieldGoalPercentage'] as num?)?.toDouble(),
+      threePointPercentage: (json['threePointPercentage'] as num?)?.toDouble(),
+      freeThrowPercentage: (json['freeThrowPercentage'] as num?)?.toDouble(),
+      effectiveFieldGoalPercentage: (json['effectiveFieldGoalPercentage'] as num?)?.toDouble(),
+      trueShootingPercentage: (json['trueShootingPercentage'] as num?)?.toDouble(),
+      turnoversPerGame: (json['turnoversPerGame'] as num?)?.toDouble(),
+      reboundsPerGame: (json['reboundsPerGame'] as num?)?.toDouble(),
+      assistsPerGame: (json['assistsPerGame'] as num?)?.toDouble(),
+      stealsPerGame: (json['stealsPerGame'] as num?)?.toDouble(),
+      blocksPerGame: (json['blocksPerGame'] as num?)?.toDouble(),
       sourceId: json['sourceId'] as String?,
       asOf: json['asOf'] as String?,
     );
@@ -50,6 +72,17 @@ class TeamSeasonStat {
   final double? offensiveRating;
   final double? defensiveRating;
   final double? netRating;
+  final double? personalFoulsPerGame;
+  final double? fieldGoalPercentage;
+  final double? threePointPercentage;
+  final double? freeThrowPercentage;
+  final double? effectiveFieldGoalPercentage;
+  final double? trueShootingPercentage;
+  final double? turnoversPerGame;
+  final double? reboundsPerGame;
+  final double? assistsPerGame;
+  final double? stealsPerGame;
+  final double? blocksPerGame;
   final String? sourceId;
   final String? asOf;
 }
