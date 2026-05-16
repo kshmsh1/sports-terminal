61df5aedb2a85ace1cfd49cf5c2fdb946c15c891
Keshav Ramesh <63920717+kshmsh1@users.noreply.github.com>
2026-05-16 00:15:55 -0500

    Add JSON serialization to team model

--- lib/models/team.dart
+++ lib/models/team.dart
@@ -8,10 +8,32 @@ class Team {
     required this.division,
   });
 
+  factory Team.fromJson(Map<String, dynamic> json) {
+    return Team(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      abbreviation: json['abbreviation'] as String,
+      city: json['city'] as String,
+      conference: json['conference'] as String,
+      division: json['division'] as String,
+    );
+  }
+
   final String id;
   final String name;
   final String abbreviation;
   final String city;
   final String conference;
   final String division;
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'abbreviation': abbreviation,
+      'city': city,
+      'conference': conference,
+      'division': division,
+    };
+  }
 }
