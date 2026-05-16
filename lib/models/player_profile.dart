a1539e890fc3b9c247c4d80966c39f85f0afdfa6
Keshav Ramesh <63920717+kshmsh1@users.noreply.github.com>
2026-05-16 00:25:02 -0500

    Add JSON serialization to player profile model

--- lib/models/player_profile.dart
+++ lib/models/player_profile.dart
@@ -20,6 +20,29 @@ class PlayerProfile {
     this.asOf,
   });
 
+  factory PlayerProfile.fromJson(Map<String, dynamic> json) {
+    return PlayerProfile(
+      id: json['id'] as String,
+      displayName: json['displayName'] as String,
+      firstName: json['firstName'] as String?,
+      lastName: json['lastName'] as String?,
+      position: json['position'] as String?,
+      height: json['height'] as String?,
+      weightPounds: json['weightPounds'] as int?,
+      birthDate: json['birthDate'] as String?,
+      birthCountry: json['birthCountry'] as String?,
+      college: json['college'] as String?,
+      draftYear: json['draftYear'] as int?,
+      draftRound: json['draftRound'] as int?,
+      draftPick: json['draftPick'] as int?,
+      nbaDebutYear: json['nbaDebutYear'] as int?,
+      isActive: json['isActive'] as bool?,
+      primaryTeamAbbreviation: json['primaryTeamAbbreviation'] as String?,
+      sourceId: json['sourceId'] as String?,
+      asOf: json['asOf'] as String?,
+    );
+  }
+
   final String id;
   final String displayName;
   final String? firstName;
@@ -38,4 +61,27 @@ class PlayerProfile {
   final String? primaryTeamAbbreviation;
   final String? sourceId;
   final String? asOf;
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'displayName': displayName,
+      'firstName': firstName,
+      'lastName': lastName,
+      'position': position,
+      'height': height,
+      'weightPounds': weightPounds,
+      'birthDate': birthDate,
+      'birthCountry': birthCountry,
+      'college': college,
+      'draftYear': draftYear,
+      'draftRound': draftRound,
+      'draftPick': draftPick,
+      'nbaDebutYear': nbaDebutYear,
+      'isActive': isActive,
+      'primaryTeamAbbreviation': primaryTeamAbbreviation,
+      'sourceId': sourceId,
+      'asOf': asOf,
+    };
+  }
 }
