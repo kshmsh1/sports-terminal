cd1c16c83c39f28e2fe14c6b673f6373f3fb865a
Keshav Ramesh <63920717+kshmsh1@users.noreply.github.com>
2026-05-16 12:26:01 -0500

    Add contract concept model

---
+++ lib/models/contract_concept.dart
@@ -0,0 +1,19 @@
+class ContractConcept {
+  const ContractConcept({
+    required this.id,
+    required this.area,
+    required this.priority,
+    required this.status,
+    required this.description,
+    required this.requiredData,
+    required this.displayUse,
+  });
+
+  final String id;
+  final String area;
+  final String priority;
+  final String status;
+  final String description;
+  final String requiredData;
+  final String displayUse;
+}
