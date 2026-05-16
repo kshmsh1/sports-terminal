43bbe8023d0ba27fa041f4dbdee10185e7e91983
Keshav Ramesh <63920717+kshmsh1@users.noreply.github.com>
2026-05-16 01:28:02 -0500

    Add JSON parsing to game record

--- lib/models/game_record.dart
+++ lib/models/game_record.dart
@@ -14,6 +14,23 @@ class GameRecord {
     this.asOf,
   });
 
+  factory GameRecord.fromJson(Map<String, dynamic> json) {
+    return GameRecord(
+      id: json['id'] as String,
+      seasonId: json['seasonId'] as String,
+      gameDate: json['gameDate'] as String?,
+      seasonType: json['seasonType'] as String?,
+      homeTeamId: json['homeTeamId'] as String?,
+      awayTeamId: json['awayTeamId'] as String?,
+      homeScore: json['homeScore'] as int?,
+      awayScore: json['awayScore'] as int?,
+      arena: json['arena'] as String?,
+      city: json['city'] as String?,
+      sourceId: json['sourceId'] as String?,
+      asOf: json['asOf'] as String?,
+    );
+  }
+
   final String id;
   final String seasonId;
   final String? gameDate;
