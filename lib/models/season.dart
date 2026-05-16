be9f81943ad0bb7e0e56dc76b854c346f364b4ab
Keshav Ramesh <63920717+kshmsh1@users.noreply.github.com>
2026-05-16 00:16:17 -0500

    Add JSON serialization to season model

--- lib/models/season.dart
+++ lib/models/season.dart
@@ -7,9 +7,29 @@ class Season {
     required this.league,
   });
 
+  factory Season.fromJson(Map<String, dynamic> json) {
+    return Season(
+      id: json['id'] as String,
+      label: json['label'] as String,
+      startYear: json['startYear'] as int,
+      endYear: json['endYear'] as int,
+      league: json['league'] as String,
+    );
+  }
+
   final String id;
   final String label;
   final int startYear;
   final int endYear;
   final String league;
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'label': label,
+      'startYear': startYear,
+      'endYear': endYear,
+      'league': league,
+    };
+  }
 }
