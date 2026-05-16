a0ad4d90a8f15c1251e91b6e2f94a0f3f43bb32f
Keshav Ramesh <63920717+kshmsh1@users.noreply.github.com>
2026-05-15 20:14:38 -0500

    Update team model for real NBA team directory

--- lib/models/team.dart
+++ lib/models/team.dart
@@ -3,21 +3,15 @@ class Team {
     required this.id,
     required this.name,
     required this.abbreviation,
+    required this.city,
     required this.conference,
-    required this.wins,
-    required this.losses,
-    required this.offensiveRating,
-    required this.defensiveRating,
-    required this.netRating,
+    required this.division,
   });
 
   final String id;
   final String name;
   final String abbreviation;
+  final String city;
   final String conference;
-  final int wins;
-  final int losses;
-  final double offensiveRating;
-  final double defensiveRating;
-  final double netRating;
+  final String division;
 }
