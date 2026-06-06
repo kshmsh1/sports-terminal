44849bc4bdcfcb434b0608beff0a5a891251804a
Keshav Ramesh <63920717+kshmsh1@users.noreply.github.com>
2026-06-06 11:34:59 -0500

    Add remaining detail validator tests to pre-data runner

--- tools/run_pre_data_tests.sh
+++ tools/run_pre_data_tests.sh
@@ -10,6 +10,10 @@ flutter test test/team_season_stat_validator_test.dart
 flutter test test/standings_record_validator_test.dart
 flutter test test/playoff_series_validator_test.dart
 flutter test test/award_record_validator_test.dart
+flutter test test/game_record_validator_test.dart
+flutter test test/roster_entry_validator_test.dart
+flutter test test/draft_pick_validator_test.dart
+flutter test test/transaction_record_validator_test.dart
 flutter test test/early_data_wave_readiness_service_test.dart
 
 echo "Pre-data test suite passed."
