cc958e68c0b3b7c6cf75ab5326f81aa870e03f0e
Keshav Ramesh <63920717+kshmsh1@users.noreply.github.com>
2026-06-06 11:30:54 -0500

    Add context validator tests to pre-data runner

--- tools/run_pre_data_tests.sh
+++ tools/run_pre_data_tests.sh
@@ -7,6 +7,9 @@ flutter test test/player_identity_import_readiness_service_test.dart
 flutter test test/search_route_payload_player_producer_test.dart
 flutter test test/player_season_stat_validator_test.dart
 flutter test test/team_season_stat_validator_test.dart
+flutter test test/standings_record_validator_test.dart
+flutter test test/playoff_series_validator_test.dart
+flutter test test/award_record_validator_test.dart
 flutter test test/early_data_wave_readiness_service_test.dart
 
 echo "Pre-data test suite passed."
