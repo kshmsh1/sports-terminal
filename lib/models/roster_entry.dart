4a782be94add87d8d5c3514f95f2734129fc97be
Keshav Ramesh <63920717+kshmsh1@users.noreply.github.com>
2026-06-08 19:52:18 -0500

    Track roster snapshot metadata on model

--- lib/models/roster_entry.dart
+++ lib/models/roster_entry.dart
@@ -11,10 +11,12 @@ class RosterEntry {
     this.endDate,
     this.sourceId,
     this.asOf,
+    this.snapshotLabel,
     this.age,
     this.height,
     this.weightPounds,
     this.college,
+    this.from,
     this.salaryUsd,
     this.salaryDisplay,
   });
@@ -32,10 +34,12 @@ class RosterEntry {
       endDate: json['endDate'] as String?,
       sourceId: json['sourceId'] as String?,
       asOf: json['asOf'] as String?,
+      snapshotLabel: json['snapshotLabel'] as String?,
       age: json['age'] as int?,
       height: json['height'] as String?,
       weightPounds: json['weightPounds'] as int?,
       college: json['college'] as String?,
+      from: json['from'] as String?,
       salaryUsd: json['salaryUsd'] as int?,
       salaryDisplay: json['salaryDisplay'] as String?,
     );
@@ -52,10 +56,14 @@ class RosterEntry {
   final String? endDate;
   final String? sourceId;
   final String? asOf;
+  final String? snapshotLabel;
   final int? age;
   final String? height;
   final int? weightPounds;
   final String? college;
+  final String? from;
   final int? salaryUsd;
   final String? salaryDisplay;
+
+  String? get fromDisplay => from ?? college;
 }
