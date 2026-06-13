be71c13bb22362b9a8ac9ec2a6e4b00fa83362c8
Keshav Ramesh <63920717+kshmsh1@users.noreply.github.com>
2026-06-13 13:57:45 -0500

    Implement login and mode routing gate

--- lib/widgets/app_entry_gate.dart
+++ lib/widgets/app_entry_gate.dart
@@ -1,8 +1,39 @@
 import 'package:flutter/material.dart';
 
+import '../controllers/auth_controller.dart';
+import '../controllers/internal_workspace_controller.dart';
+import '../screens/login_screen.dart';
+import 'terminal_shell.dart';
+import 'user_terminal_shell.dart';
+
 class AppEntryGate extends StatelessWidget {
-  const AppEntryGate({super.key});
+  const AppEntryGate({
+    super.key,
+    required this.authController,
+    required this.workspaceController,
+  });
+
+  final AuthController authController;
+  final InternalWorkspaceController workspaceController;
 
   @override
-  Widget build(BuildContext context) => const SizedBox.shrink();
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: authController,
+      builder: (context, _) {
+        final session = authController.session;
+        if (session == null) {
+          return LoginScreen(controller: authController);
+        }
+        if (session.role.canAccessPlatformAdmin) {
+          return const TerminalShell();
+        }
+        return UserTerminalShell(
+          session: session,
+          workspaceController: workspaceController,
+          onSignOut: authController.signOut,
+        );
+      },
+    );
+  }
 }
