2a29d89d4561a60577bdf32967c7daf6f548eb75
Keshav Ramesh <63920717+kshmsh1@users.noreply.github.com>
2026-06-13 14:11:49 -0500

    Test responsive login layout

--- test/user_mode_layout_test.dart
+++ test/user_mode_layout_test.dart
@@ -1,7 +1,19 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:sports_terminal/controllers/auth_controller.dart';
+import 'package:sports_terminal/screens/login_screen.dart';
 
 void main() {
-  test('layout test shell', () {
-    expect(true, isTrue);
+  testWidgets('login renders at 720 pixel width', (tester) async {
+    await tester.binding.setSurfaceSize(const Size(720, 900));
+    addTearDown(() => tester.binding.setSurfaceSize(null));
+
+    await tester.pumpWidget(
+      MaterialApp(home: LoginScreen(controller: AuthController())),
+    );
+    await tester.pump();
+
+    expect(find.text('Sports Terminal'), findsOneWidget);
+    expect(tester.takeException(), isNull);
   });
 }
