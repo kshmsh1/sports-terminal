e8c9f277ed69261abbdfe10095a22162ab38bb5e
Keshav Ramesh <63920717+kshmsh1@users.noreply.github.com>
2026-06-13 13:55:16 -0500

    Add safe SQL screen shell

---
+++ lib/screens/safe_sql_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SafeSqlScreen extends StatelessWidget {
+  const SafeSqlScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('SQL workspace');
+  }
+}
