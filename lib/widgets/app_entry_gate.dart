abf0fc881c7e9927cc97c264c8daaa49020a3dc0
Keshav Ramesh <63920717+kshmsh1@users.noreply.github.com>
2026-06-13 13:57:27 -0500

    Add app entry gate shell

---
+++ lib/widgets/app_entry_gate.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppEntryGate extends StatelessWidget {
+  const AppEntryGate({super.key});
+
+  @override
+  Widget build(BuildContext context) => const SizedBox.shrink();
+}
