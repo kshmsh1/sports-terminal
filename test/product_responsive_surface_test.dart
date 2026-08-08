1122629139f3176a6625355c2ac5d960b60e08a3
Keshav Ramesh <63920717+kshmsh1@users.noreply.github.com>
2026-08-07 19:45:44 -0500

    Cover entity intelligence responsive mounting

--- test/product_responsive_surface_test.dart
+++ test/product_responsive_surface_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:sports_terminal/screens/product_nba_entity_command_center_screen.dart';
 import 'package:sports_terminal/screens/product_nba_stats_center_screen.dart';
 import 'package:sports_terminal/screens/product_trade_machine_screen.dart';
 
@@ -76,6 +77,30 @@ void main() {
     );
 
     expect(find.byType(ProductNbaStatsCenterScreen), findsOneWidget);
+    expect(tester.takeException(), isNull);
+  });
+
+  testWidgets('entity command center mounts at narrow width', (tester) async {
+    await pumpSurface(
+      tester,
+      const ProductNbaEntityCommandCenterScreen(),
+      const Size(820, 1000),
+    );
+
+    expect(find.byType(ProductNbaEntityCommandCenterScreen), findsOneWidget);
+    expect(find.text('NBA ENTITY & SEASON INTELLIGENCE'), findsOneWidget);
+    expect(tester.takeException(), isNull);
+  });
+
+  testWidgets('entity command center mounts at desktop width', (tester) async {
+    await pumpSurface(
+      tester,
+      const ProductNbaEntityCommandCenterScreen(),
+      const Size(1440, 1100),
+    );
+
+    expect(find.byType(ProductNbaEntityCommandCenterScreen), findsOneWidget);
+    expect(find.text('NBA ENTITY & SEASON INTELLIGENCE'), findsOneWidget);
     expect(tester.takeException(), isNull);
   });
 }
