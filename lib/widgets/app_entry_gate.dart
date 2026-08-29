8d834042f1412b201a458d799494bb570d7df27e
Keshav Ramesh <63920717+kshmsh1@users.noreply.github.com>
2026-08-29 15:12:51 -0500

    Mount one canonical traditional website shell

--- lib/widgets/app_entry_gate.dart
+++ lib/widgets/app_entry_gate.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import '../controllers/auth_controller.dart';
 import '../controllers/internal_workspace_controller.dart';
 import '../screens/login_screen.dart';
-import 'traditional_website_shell_v2.dart';
+import 'traditional_website_shell.dart';
 
 class AppEntryGate extends StatelessWidget {
   const AppEntryGate({
@@ -28,7 +28,7 @@ class AppEntryGate extends StatelessWidget {
           return LoginScreen(controller: authController);
         }
 
-        return TraditionalWebsiteShellV2(
+        return TraditionalWebsiteShell(
           session: session,
           workspaceController: workspaceController,
           onSignOut: authController.signOut,
