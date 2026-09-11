a2d31a3541e24f6af5c63dcb28f87cbe24a995ee
Keshav Ramesh <63920717+kshmsh1@users.noreply.github.com>
2026-09-11 14:42:09 -0500

    Route Trade Machine entry point to complete workbench

--- lib/screens/product_trade_machine_screen.dart
+++ lib/screens/product_trade_machine_screen.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
-import 'product_trade_machine_2026_screen.dart';
+import 'product_trade_machine_complete_screen.dart';
 
 /// Stable public shell entry point for the Sports Terminal NBA Trade Machine.
 class ProductTradeMachineScreen extends StatelessWidget {
   const ProductTradeMachineScreen({super.key});
 
   @override
-  Widget build(BuildContext context) => const ProductTradeMachine2026Screen();
+  Widget build(BuildContext context) => const ProductTradeMachineCompleteScreen();
 }
