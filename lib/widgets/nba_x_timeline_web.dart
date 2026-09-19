4c20ed751172ee157c0d58662f1ead73011df888
Keshav Ramesh <63920717+kshmsh1@users.noreply.github.com>
2026-09-19 18:21:20 -0500

    Disable third-party social embed widget

--- lib/widgets/nba_x_timeline_web.dart
+++ lib/widgets/nba_x_timeline_web.dart
@@ -1,9 +1,6 @@
-import 'dart:ui_web' as ui_web;
-
 import 'package:flutter/material.dart';
-import 'package:web/web.dart' as web;
 
-class NbaXTimeline extends StatefulWidget {
+class NbaXTimeline extends StatelessWidget {
   const NbaXTimeline({
     super.key,
     required this.handle,
@@ -16,59 +13,18 @@ class NbaXTimeline extends StatefulWidget {
   final double height;
 
   @override
-  State<NbaXTimeline> createState() => _NbaXTimelineState();
-}
-
-class _NbaXTimelineState extends State<NbaXTimeline> {
-  late String _viewType;
-
-  @override
-  void initState() {
-    super.initState();
-    _register();
-  }
-
-  @override
-  void didUpdateWidget(covariant NbaXTimeline oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    if (oldWidget.handle != widget.handle) _register();
-  }
-
-  void _register() {
-    final token = '${widget.handle}-${DateTime.now().microsecondsSinceEpoch}';
-    _viewType = 'sports-terminal-x-$token';
-    ui_web.platformViewRegistry.registerViewFactory(_viewType, (int viewId) {
-      final wrapper = web.HTMLDivElement()
-        ..style.width = '100%'
-        ..style.height = '${widget.height}px'
-        ..style.overflow = 'auto'
-        ..style.backgroundColor = 'transparent';
-
-      final anchor = web.HTMLAnchorElement()
-        ..href = 'https://twitter.com/${widget.handle}'
-        ..className = 'twitter-timeline'
-        ..textContent = 'Posts by ${widget.displayName}'
-        ..setAttribute('data-theme', 'dark')
-        ..setAttribute('data-chrome', 'nofooter')
-        ..setAttribute('data-dnt', 'true');
-      wrapper.append(anchor);
-
-      final script = web.HTMLScriptElement()
-        ..src = 'https://platform.twitter.com/widgets.js'
-        ..async = true
-        ..charset = 'utf-8';
-      wrapper.append(script);
-      return wrapper;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) => SizedBox(
-        key: ValueKey(_viewType),
-        height: widget.height,
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(12),
-          child: HtmlElementView(viewType: _viewType),
+        height: height,
+        child: Card(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(24),
+              child: Text(
+                'Third-party social embeds are disabled in the static Sports Terminal runtime. @$handle ($displayName) remains available as descriptive source metadata only.',
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
         ),
       );
 }
