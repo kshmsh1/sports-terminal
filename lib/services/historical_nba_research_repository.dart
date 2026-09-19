a194e122170d783a48c0e0c5f6bcb26e67e28f8b
Keshav Ramesh <63920717+kshmsh1@users.noreply.github.com>
2026-09-19 18:25:49 -0500

    Remove unused backend store fields

--- lib/services/historical_nba_research_repository.dart
+++ lib/services/historical_nba_research_repository.dart
@@ -6,9 +6,7 @@ import 'product_local_store.dart';
 class HistoricalNbaResearchRepository {
   const HistoricalNbaResearchRepository({
     ProductLocalStore store = const ProductLocalStore(),
-  }) : _store = store;
-
-  final ProductLocalStore _store;
+  });
 
   Future<Map<String, dynamic>> summary() =>
       _get('/v2/nba/history/research/summary');
